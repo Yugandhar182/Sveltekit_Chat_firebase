004f3b91f1c7cbe78540efbe6260bd840be8d772
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-03 15:01:07 +0530

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,324 @@
+<script>
+	import { onMount } from 'svelte';
+	import { getDatabase, ref, push, onValue, remove } from 'firebase/database';
+    import firebaseApp from './firebase.js';
+    import { Input, Label, Helper, Button, Card } from 'flowbite-svelte';
+
+
+	
+  
+	let fullName = '';
+	let message = '';
+	let messages = [];
+	let onlineUsers = [];
+	let joinedChat = false;
+	
+   
+  
+	// Firebase Realtime Database references
+	const db = getDatabase(firebaseApp);
+	const messagesRef = ref(db, 'messages');
+	const onlineUsersRef = ref(db, 'onlineUsers');
+	// Function to send a new message
+	function sendMessage() {
+	  if (message.trim() !== '') {
+		push(messagesRef, {
+		  fullName,
+		  message,
+		  timestamp: {
+			'.sv': 'timestamp', // ServerValue.TIMESTAMP equivalent in v9
+		  },
+		});
+		message = '';
+	  }
+	}
+  
+	function formatDate(timestamp) {
+	const date = new Date(timestamp);
+	let hours = date.getHours();
+	const minutes = date.getMinutes();
+	const ampm = hours >= 12 ? 'pm' : 'am';
+	hours %= 12;
+	hours = hours || 12;
+	const formattedTime = `${hours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+	return formattedTime;
+  }
+  
+	// Function to add a new online user
+	function addOnlineUser() {
+	  if (fullName.trim() !== '') {
+		push(onlineUsersRef, { fullName }).then(() => {
+		  joinedChat = true; // Set the flag to indicate user joined the chat
+		});
+	  }
+	}
+  
+  
+  
+	// Listen for new messages and online users
+	onMount(() => {
+	  onValue(messagesRef, (snapshot) => {
+		messages = Object.values(snapshot.val() || {});
+	  });
+  
+	  onValue(onlineUsersRef, (snapshot) => {
+		onlineUsers = Object.values(snapshot.val() || {});
+		console.log("Online Users", onlineUsers);
+	  });
+	});
+  
+   
+   
+  
+	// Function to clear all messages from the chat
+	function clearData() {
+	  // Remove all messages
+	  const messagesRef = ref(db, 'messages');
+	  remove(messagesRef);
+	}
+  
+	function showUserMessages(selectedUserName) {
+	  fullName = selectedUserName;
+	  // Filter the messages based on the selected user's name
+	  messages = messages.filter(
+		(messageData) => messageData.fullName === fullName
+	  );
+	}
+	
+	let messageContainer; 
+  
+	function scrollToBottom() {
+	  if (messageContainer) {
+		messageContainer.scrollTop = messageContainer.scrollHeight;
+	  }
+	}
+  
+	onMount(scrollToBottom);
+  
+  
+  
+  
+  </script>
+	
+	  {#if !joinedChat}
+
+          <div>
+            <Card img="https://images.unsplash.com/photo-1636751364472-12bfad09b451?" horizontal class="mb-4" style="width:1000px;margin-left:400px; margin-top:100px">
+                <h1 style="color: red; font-weight:bold;font-size:20px">Enter your name to join the Chat</h1>
+               
+                  <Input  type="text" id="fullName" bind:value={fullName} placeholder="Enter your name"  style="width:350px;height:40px; margin-top:20px;border: 1px solid  blue;font-weight:bold" />
+                  <Button on:click={addOnlineUser} style="color:white;background-color:blue;margin-top:20px">Join Chat</Button>
+                
+            </Card>
+            </div>
+
+		  {:else}
+  
+          <div class="main-container">
+             <div class="message-container" bind:this={messageContainer}>
+				{#each messages as messageData}
+                <div class="chat-message {messageData.fullName === fullName ? 'sender' : 'receiver'}">
+                {#if messageData.fullName !== fullName}
+               <img src={`https://ui-avatars.com/api/?name=${encodeURIComponent(messageData.fullName)}&background=random`} alt="{messageData.fullName}" class="receiver-avatar" />
+               {/if}
+   
+              <span   class="chat-sender" >{messageData.fullName}:</span>
+   
+             <span style="color:blue;font-weight:bold" class="chat-content">{messageData.message}</span>
+             <span class="chat-timestamp">{formatDate(messageData.timestamp)}</span>
+             </div>
+            {/each}
+
+            </div>
+            <div class="onlineusers-container">
+                <h1 style="color: green; margin-left:40px;font-weight: bold;font-size:30px">Online users</h1>
+               <hr>
+                {#each onlineUsers as user}
+                <div class="card-body" style="width: 220px; overflow-x: auto; overflow-y: hidden; height: 40px; margin-left:10px; margin-top:20px">
+                   
+                  <p style="color: black;margin-left:40px" class="username"  >{user.fullName}</p>
+                  <img src={`https://ui-avatars.com/api/?name=${encodeURIComponent(user.fullName)}&background=random`} alt="{user.fullName}" class="online-avatar" />
+                </div>
+              {/each}
+            </div>
+          
+    
+
+          </div>
+          <div class="input-container">
+
+            <Input  bind:value={message} type="text"  placeholder="Write messages" style=" width:900px; height:43px; color:blue; border: 1px solid  black ; font-weight:bold" />
+
+        </div>
+        <div class="button-container">
+            <Button   on:click={sendMessage}  >Send</Button>
+          </div>
+        {/if}
+
+
+	  
+  
+  <style>
+ 
+ .sender-avatar {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 10px;
+    background-color: blueviolet;
+    float: right; /* Align sender avatars to the right */
+  }
+
+  .receiver-avatar {
+    width: 30px;
+    height: 30px;
+    
+    border-radius: 50%;
+    margin-right: 10px;
+    background-color: blueviolet;
+    float: right; /* Align receiver avatars to the left */
+  }
+  .sending-avatar {
+    width: 30px;
+    height: 30px;
+    
+    border-radius: 50%;
+    margin-right: 10px;
+    background-color: blueviolet;
+    float: right; /* Align receiver avatars to the left */
+  }
+  
+  .online-avatar {
+    width: 30px;
+    height: 30px;
+    margin-top: -28px;
+    border-radius: 50%;
+    margin-right: 10px;
+    background-color: blueviolet;
+    float:left; /* Align receiver avatars to the left */
+  }
+
+  .chat-message.sender {
+    color: black;
+    background-color: #e0e0e0; /* Add a background color to sender messages */
+    margin-left: auto;
+  }
+
+  .chat-message.receiver {
+    color: blue;
+    background-color: #f0f0f0; /* Add a background color to receiver messages */
+    margin-right:-100px;
+  }
+
+.button-container{
+
+margin-left: 1007px;
+width: 70px;
+margin-top: -45px;
+
+}
+.onlineusers-container{
+    width: 270px;
+    height: 650px;
+    border: 1px solid  rgb(76, 21, 227); /* Add a border for styling */
+    margin-top:-650px;
+    overflow-y: auto; 
+  position: relative;
+  margin-left: 10px;
+ 
+}
+  
+ 
+  
+	.input-container  {
+
+    margin-top: -48px;
+    height: 48px;
+    width: 980px;
+    margin-left: 410px;
+
+  
+	}
+   
+	
+	.button-container{
+	
+	  margin-bottom:1px;
+      background-color: blue;
+      margin-left: 1320px;
+      border: 1px;
+	}
+  .main-container{
+	  margin-left: 110px;
+	  margin-top:10px;
+   width: 1315px;
+   height: 720px;
+	
+    border: 2px solid  white; /* Add a border for styling */
+
+	}
+	.message-container {
+	 
+	   border-radius: 5px;
+	   margin-top: 10px;
+		margin-left:299px;
+		height: 650px;
+		border: 0px solid #ccc;
+	  width:985px;
+	  border: 1px solid  rgb(13, 212, 79); /* Add a border for styling */
+	  justify-content: center;
+	
+	  overflow: auto;
+	  }
+  .chat-message {
+	  margin: 5px;
+	  border: 1px solid #ccc;
+	  border-radius: 5px;
+	  padding: 10px;
+	  display: flex;
+	  align-items: center;
+	  width: 480px;
+	  margin-left:20px;
+	  height:70px;
+	 
+	}
+  
+	.chat-sender {
+	  font-weight: bold;
+	  margin-right: 10px;
+	  font-size: 14px;
+      height: 20px;
+	
+	}
+  
+	.chat-timestamp {
+	  margin-left: auto;
+	}
+  
+	.chat-message.sender {
+	 
+	  color: black; 
+	}
+  
+	.chat-message.receiver {
+	 
+	  color: blue;
+	}
+	.chat-content{
+	  font-size:17px;
+	
+	}
+
+    .card-body {
+	  padding: 10px;
+	  border: 1px solid #ccc;
+	  border-radius: 5px;
+	  margin-bottom: 5px;
+	
+	  height: 40px;
+	}
+  
+   
+ 
+
+  </style>
